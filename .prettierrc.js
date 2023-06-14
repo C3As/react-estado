69e56c1a3cc3865625cb88a1896793f884f7fd29
Carlos Andrade <63912176+C3As@users.noreply.github.com>
2023-06-14 14:15:56 +0100

    fim cap1

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    printWidth: 80,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 4,
+    useTabs: false,
+    trailingComma: 'es5',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'avoid',
+  };
---
+++ src/components/template/MenuItem.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import React from "react";
+
+interface MenuItemProps {
+    texto: string;
+    url: string;
+    icone: any;
+}
+
+export default function MenuItem(props: MenuItemProps) {
+    return (
+       <Link href={props.url} className="flex flex-col items-center gap-2">
+            <div className="bg-gradient-to-r from-blue-500 to-green-500 rounded-full p-5">
+                {React.cloneElement(props.icone, {size:90, stroke: 1, className: 'opacity-40' })}
+            </div>
+            <span className="opacity-40 font-black text-3xl text-white">{props.texto}</span>
+       </Link>
+    )
+}
